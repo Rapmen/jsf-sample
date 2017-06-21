6faaccee3652b7719703d50ec9205946ed0395cc
Rapmen <rapmenwell@gmail.com>
2017-06-21 15:17:38 +0800

    create jsf-sample

---
+++ src/main/java/com/accentrix/index/IndexBean.java
@@ -0,0 +1,14 @@
+package com.accentrix.index;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class IndexBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
