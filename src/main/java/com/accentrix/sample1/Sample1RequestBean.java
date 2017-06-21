9c22b33f836cf115030a6a5e207e19b959431194
Rapmen <rapmenwell@gmail.com>
2017-06-21 19:14:27 +0800

    modify sample1

--- src/main/java/com/accentrix/sample1/Sample1RequestBean.java
+++ src/main/java/com/accentrix/sample1/Sample1RequestBean.java
@@ -1,10 +1,32 @@
 package com.accentrix.sample1;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 @ManagedBean
 @RequestScoped
 public class Sample1RequestBean {
+	
+	@ManagedProperty("sample1View")
+	private Sample1View sample1View;
+	
+	@PostConstruct
+	private void init(){
+		System.out.println("Sample1RequestBean init======");
+	}
+	
+	public void onSubmit(){
+		System.out.println(sample1View.getText());
+	}
+
+	public Sample1View getSample1View() {
+		return sample1View;
+	}
+
+	public void setSample1View(Sample1View sample1View) {
+		this.sample1View = sample1View;
+	}
 
 }
--- src/main/java/com/accentrix/sample1/Sample1View.java
+++ src/main/java/com/accentrix/sample1/Sample1View.java
@@ -2,6 +2,7 @@ package com.accentrix.sample1;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -10,5 +11,20 @@ import javax.faces.bean.ViewScoped;
 public class Sample1View implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private String text;
+	
+	@PostConstruct
+	private void init(){
+		System.out.println("Sample1View init=======");
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
 
 }
